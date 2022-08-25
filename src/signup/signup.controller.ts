4606298470bb664eed067fd1b28139c48316cc98
kennedy7 <kennedyhillary6@gmail.com>
2022-08-25 22:51:22 +0100

    signup resources

--- src/signup/signup.controller.ts
+++ src/signup/signup.controller.ts
@@ -3,7 +3,7 @@ import { SignupService } from './signup.service';
 import { CreateSignupDto } from './dto/create-signup.dto';
 import { UpdateSignupDto } from './dto/update-signup.dto';
 
-@Controller('signup')
+@Controller('signup') //http://localhost:8000/signup
 export class SignupController {
   constructor(private readonly signupService: SignupService) {}
 
