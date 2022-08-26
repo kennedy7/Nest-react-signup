fd0fc378e69941c268980ebf097c747c828686b0
kennedy7 <kennedyhillary6@gmail.com>
2022-08-26 21:48:30 +0100

    signup resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,9 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { SignupModule } from './signup/signup.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017/react'), SignupModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/react'),
+   SignupModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
