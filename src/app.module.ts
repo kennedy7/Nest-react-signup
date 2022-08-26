72659e50e7e9e1cef759fe5f42bdda3c5ebd03b2
kennedy7 <kennedyhillary6@gmail.com>
2022-08-26 21:46:33 +0100

    signup resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { SignupModule } from './signup/signup.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017'), SignupModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017/react'), SignupModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/signup/signup.module.ts
+++ src/signup/signup.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { SignupService } from './signup.service';
 import { SignupController } from './signup.controller';
-import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseModule, Schema } from '@nestjs/mongoose';
+import { SignupSchema } from './signup.model';
 
 @Module({
-  imports: [MongooseModule.forFeature('')],
+  imports: [
+    MongooseModule.forFeature([
+    {
+       name: "Signup",
+      schema: SignupSchema
+    }
+  ])
+],
   controllers: [SignupController],
   providers: [SignupService],
 })
