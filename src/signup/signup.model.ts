c8c6a8407bfd3d417d7c9258ad3ac1cb859200da
kennedy7 <kennedyhillary6@gmail.com>
2022-08-26 20:13:26 +0100

    signup resources

---
+++ src/signup/signup.model.ts
@@ -0,0 +1,21 @@
+import { Schema, Prop, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+
+
+@Schema()
+export class Signup { 
+    @Prop({unique: true})
+    username: string;
+
+    @Prop({unique: true})
+    email: string;
+
+    @Prop()
+    password: string;
+
+    
+}
+
+export type SignupModel = Signup & Document;
+
+export const SignupSchema = SchemaFactory.createForClass(Signup);
--- src/signup/signup.module.ts
+++ src/signup/signup.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { SignupService } from './signup.service';
 import { SignupController } from './signup.controller';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [MongooseModule.forFeature('')],
   controllers: [SignupController],
-  providers: [SignupService]
+  providers: [SignupService],
 })
 export class SignupModule {}
